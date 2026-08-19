26de2f648439222241134670c7f450374e7a252b
Aldy Rahman <aldy.rahman02@gmail.com>
2026-08-19 13:38:34 +0800

    Feat: Setup Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return (
+    <div className='text-2xl text-blue-600'>Test</div>
+  )
+}
+
+export default App
