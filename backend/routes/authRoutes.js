a5c205b5b598d4e6cc81b78fe7c28ca86cbef69c
Aldy Rahman <aldy.rahman02@gmail.com>
2026-08-26 10:10:37 +0800

    Fix: Add authRoutes (Forget To Push)

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,6 +7,8 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password/:token', authController.resetPassword);
+router.post('/logout', authController.logout);
+router.get('/me', verifyToken, authController.getMe);
 
 // Pengaturan Akun
 router.get('/profile', verifyToken, authController.getProfile);
